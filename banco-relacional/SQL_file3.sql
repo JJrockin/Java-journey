f429821833cb5760be057ebd8c43dd0bac4ad7f0
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-04-11 08:25:19 -0300

    new query SQL

---
+++ banco-relacional/SQL_file3.sql
@@ -0,0 +1,6 @@
+select * from pessoas where nome like 'Je%';
+select * from pessoas;
+UPDATE pessoas SET nome = 'Jeronimo Moraes' WHERE codigo = 1;
+
+select * from alunobolsista;
+select * from aluno;
